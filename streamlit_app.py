cabaa65e9d691878bf04c4705d48a7edaf2788d6
jckhor <khorjohnnych@gmail.com>
2026-08-28 11:01:31 -0700

    Correct Snowflake connection and import
    Fix connection to Snowflake and update import statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 # Import python packages
 import streamlit as st
 import os
-from snowflake.snowpark.context import get_active_session
+## from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connetion("snowflake")
+session = cnx.session()                   
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
